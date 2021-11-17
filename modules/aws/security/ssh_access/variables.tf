0c27c81f4d5238ec1297ce73965057a8e1102b98
Cayo Andrade <cayo.andrade@conductor.com.br>
2021-11-17 07:41:47 -0300

    initial commit

---
+++ modules/aws/security/ssh_access/variables.tf
@@ -0,0 +1,22 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "name" {
+  type        = string
+  description = "Nome do Recurso"
+}
+
+variable "ingress" {
+  type = map(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
